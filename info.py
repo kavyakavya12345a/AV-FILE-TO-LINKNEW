9f5c007caa77c059badd232db1b440c691cd6145
kavyakavya12345a <opkavyatele@gmail.com>
2025-02-11 15:54:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ BOT_USERNAME = environ.get("BOT_USERNAME", 'Djdjfjefjbot') # without @
 BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1002452252377')) # admin your channel in stream 
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1002489352547')) # admin your channel in users log 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7154442141').split()] # 3567788, 678899, 5889467
-OWNER_USERNAME = environ.get("OWNER_USERNAME", 'HELLOW_BOB) # without @ 
+OWNER_USERNAME = environ.get("OWNER_USERNAME", 'hellow_bob193) # without @ 
 
 # pics information
 PICS = environ.get('PICS', 'https://envs.sh/b6H.jpg')
